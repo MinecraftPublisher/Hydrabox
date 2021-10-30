b28f7c842051118b6dea87fe833691e38f16dacb
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2021-10-30 10:16:30 +0330

    details declaration.

--- hydrabox.js
+++ hydrabox.js
@@ -39,8 +39,9 @@ let Hydra = (function() {
       Hydra.warning('Hydra was unable to detect your fingerprint, Please click <a href="https://hydrabox.phazor.ir/API/Fingerprint/">Here</a> to set it.')
       return 'NULL'
     } else {
+      var details = {'fingerprint': fingerprint, 'data': JSON.stringify(value) }
       var formBody = [];
-      for (var property in {'fingerprint': fingerprint, 'data': JSON.stringify(value) }) {
+      for (var property in details) {
         var encodedKey = encodeURIComponent(property);
         var encodedValue = encodeURIComponent(details[property]);
         formBody.push(encodedKey + "=" + encodedValue);
