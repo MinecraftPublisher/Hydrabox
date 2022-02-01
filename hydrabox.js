c730c515a2051bad8039a871d8ca4785e180c19e
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2022-02-01 09:12:34 +0330

    Update hydrabox.js

--- hydrabox.js
+++ hydrabox.js
@@ -19,7 +19,7 @@ window.ProtoHydra = (async function() {
   /* END Hydrabox stylesheet */
 
   /* BEGIN Define libraries */
-  await import('https://unpkg.com/toastify@2.0.1/dist/build.js')
+  window.Toastify = (await import('https://unpkg.com/toastify@2.0.1/dist/build.js')).Toastify
   Hydrabox.write('<link rel="stylesheet" href="https://unpkg.com/toastify@2.0.1/dist/toastify.css">')
   Hydrabox.Toast = function(text) { Toastify({ text: text, duration: 3000, close: true, gravity: "bottom", position: "right", stopOnFocus: true, style: {background: "linear-gradient(to right, #00b09b, #96c93d)",}, onClick: function(){}}).showToast() }
   await import('https://unpkg.com/jquery@3.3.1/dist/jquery.min.js')
@@ -75,7 +75,7 @@ window.ProtoHydra = (async function() {
   window.Hydra = {}
   Hydrabox.Toast('HydraBox has successfully loaded.')
   return {
-    'version': '0.5.83',
+    'version': '0.5.85',
     'Hydrabox': Hydrabox,
     'API': HydraAPI
   }
